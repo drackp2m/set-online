6f4584a342dfae50c3955da9ad1d0fcea8f68cdf
drackp2m <drackiouscolinatesianos@gmail.com>
2022-07-02 12:10:21 +0200

    🚧 W.I.P.

--- apps/api/src/utils/env-schema.ts
+++ apps/api/src/utils/env-schema.ts
@@ -40,10 +40,11 @@ export interface IEnvSchema {
 	DB_NAME: string;
 	DB_USER: string;
 	DB_PASS: string;
-	JWT_ID: string;
-	JWT_ISSUER: string;
 	JWT_ALGORITHM: EnvJwtAlgorithm;
 	JWT_SECRET: string;
+	JWT_ISSUER: string;
+	JWT_AUDIENCE: string;
+	JWT_ID: string;
 	JWT_EXPIRES_IN: string;
 }
 
@@ -58,10 +59,11 @@ export const envSchema = Joi.object({
 	DB_NAME: string,
 	DB_USER: string,
 	DB_PASS: string,
-	JWT_ID: string,
-	JWT_ISSUER: string,
 	JWT_ALGORITHM: string.valid(...JWT_ALGORITHMS),
 	JWT_SECRET: string,
+	JWT_ISSUER: string,
+	JWT_AUDIENCE: string,
+	JWT_ID: string,
 	JWT_EXPIRES_IN: string,
 }).unknown();
 
