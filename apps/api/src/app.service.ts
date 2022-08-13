972003e985fc415d8e05b18f779a041616b44be7
Marc Jovaní González <drackp2m@MarcBook-Air.local>
2022-08-13 21:54:37 +0200

    🔥 update tests to match with app-service, but not works

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -2,11 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { Message } from '@set-online/api-interfaces';
 
 @Injectable()
-export class
-	AppService {
-	getData(
-
-	): Message {
-		return { message: 'Welcome to api!' };
+export class AppService {
+	getData(): Message {
+		return { message: 'Welcome to set-online!' };
 	}
 }
