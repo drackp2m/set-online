31478e10f3cd63053c18d66d2b22182502d93b68
Marc Jovaní González <drackp2m@MarcBook-Air.local>
2022-08-14 03:50:46 +0200

    🚷 remove wrong livbs, restore tsconfig jsons, updated dependencies

--- apps/api/src/config/mikro-orm.config.ts
+++ apps/api/src/config/mikro-orm.config.ts
@@ -1,18 +1,9 @@
 import { AnyEntity, LoadStrategy } from '@mikro-orm/core';
 import { MikroOrmModuleSyncOptions } from '@mikro-orm/nestjs';
-import { readdirSync, existsSync } from 'fs';
+import { existsSync, readdirSync } from 'fs';
 import { databaseConfig } from './database.config';
 
 async function getEntities(): Promise<AnyEntity[]> {
-	if (process.env.WEBPACK) {
-		const modules = require.context('../modules', true, /\.ts$/);
-
-		return modules
-			.keys()
-			.map((r) => modules(r))
-			.flatMap((mod) => Object.keys(mod).map((className) => mod[className]));
-	}
-
 	const promises = readdirSync('apps/api/src/modules')
 		.map((file) => {
 			if (existsSync(`apps/api/src/modules/${file}/${file}.entity.ts`)) {
