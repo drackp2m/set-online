a652f1e8010687e1e0015f42a7ffd9ac87994df3
drackp2m <drackiouscolinatesianos@gmail.com>
2022-09-06 00:33:27 +0200

    🫡 tested full AuthService.login

---
+++ apps/api/src/user/user.faker.ts
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker';
+import { EntityData } from '@mikro-orm/core';
+
+import { BasicFaker } from '../common/fakers/basic.faker';
+import { DateFaker } from '../common/fakers/date.faker';
+import { UserRole } from './interfaces/user-role.enum';
+import { User } from './user.entity';
+
+export interface UserFakerOptions {
+	createdFrom?: string;
+}
+
+export class UserFaker {
+	private readonly basicFaker = new BasicFaker();
+	private readonly dateFaker = new DateFaker();
+
+	make(options?: UserFakerOptions): EntityData<User> {
+		const firstName = faker.name.firstName();
+		const lastName = faker.name.lastName();
+
+		return {
+			uuid: faker.datatype.uuid(),
+			username: faker.internet.userName(firstName, lastName),
+			password: faker.internet.password(32, false, /[A-Z0-9_\-*?.]/),
+			email: this.basicFaker.boolean()
+				? faker.internet.email(firstName, lastName)
+				: undefined,
+			role: this.basicFaker.randomEnum(UserRole),
+			createdAt: this.dateFaker.createdAt(options?.createdFrom),
+			updatedAt: this.dateFaker.modifiedAt(),
+		};
+	}
+}
