e57c05dc2835861aece89406ea6598f537826cd5
drackp2m <drackiouscolinatesianos@gmail.com>
2022-09-06 00:33:27 +0200

    🫡 tested full AuthService.login

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,16 +1,24 @@
-/* eslint-disable */
 export default {
 	displayName: 'api',
 	preset: '../../jest.preset.js',
+	testEnvironment: 'node',
 	globals: {
 		'ts-jest': {
 			tsconfig: '<rootDir>/tsconfig.spec.json',
 		},
 	},
-	testEnvironment: 'node',
 	transform: {
-		'^.+\\.[tj]s$': 'ts-jest',
+		'^.+\\.[tj]s$': [
+			'ts-jest',
+			{
+				tsconfig: '<rootDir>/tsconfig.spec.json',
+				diagnostics: {
+					ignoreCodes: [151001],
+				},
+			},
+		],
 	},
 	moduleFileExtensions: ['ts', 'js', 'html'],
 	coverageDirectory: '../../coverage/apps/api',
+	coverageReporters: ['html', ['text', { skipFull: true }]],
 };
---
+++ apps/api/src/user/user.factory.ts
@@ -0,0 +1,14 @@
+import { Constructor, EntityData } from '@mikro-orm/core';
+import { Factory } from '@mikro-orm/seeder';
+
+import { User } from './user.entity';
+import { UserFaker } from './user.faker';
+
+export class UserFactory extends Factory<User> {
+	model: Constructor<User>;
+	faker: UserFaker = new UserFaker();
+
+	protected definition(): EntityData<User> {
+		return this.faker.make();
+	}
+}
