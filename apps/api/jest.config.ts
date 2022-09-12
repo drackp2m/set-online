306c18c52416c7f3de91a58ee08ef14e0786ec82
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2022-09-12 16:12:21 +0200

    🙌 deps updated to last version; migrations, tests and builds works perfect

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -2,24 +2,20 @@ export default {
 	displayName: 'api',
 	preset: '../../jest.preset.js',
 	testEnvironment: 'node',
-	globals: {
-		'ts-jest': {
-			tsconfig: '<rootDir>/tsconfig.spec.json',
-			// isolatedModules: true,
-		},
-	},
+	maxWorkers: 1,
 	transform: {
 		'^.+\\.[tj]s$': [
 			'ts-jest',
 			{
 				tsconfig: '<rootDir>/tsconfig.spec.json',
-				diagnostics: {
-					ignoreCodes: [151001],
-				},
+				diagnostics: false,
+				// isolatedModules: true,
+				// diagnostics: {
+				// 	ignoreCodes: [151001],
+				// },
 			},
 		],
 	},
-	maxWorkers: 1,
 	moduleFileExtensions: ['ts', 'js', 'html'],
 	coverageDirectory: '../../coverage/apps/api',
 	coverageReporters: ['html', ['text-summary', { skipFull: true }]],
