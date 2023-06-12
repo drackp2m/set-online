d3184e41bb45b68136e824c4e5452e5c4ee23aa5
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-06-12 23:07:23 +0000

    ✨ feat(api,app): add selfsigned ssl

--- apps/api-e2e/src/api/api.spec.ts
+++ apps/api-e2e/src/api/api.spec.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 describe('GET /api', () => {
 	it('should return a message', async () => {
-		const res = await axios.get(`/api`);
+		const res = await axios.get(`/api/hello`);
 
 		expect(res.status).toBe(200);
-		expect(res.data).toEqual({ message: 'Hello API' });
+		expect(res.data).toEqual({ message: 'Welcome to set-online!' });
 	});
 });
