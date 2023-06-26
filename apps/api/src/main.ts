7ce4e4a3de52b4ac36e30eeeaf2bb9a637b5a4c6
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-06-26 21:55:55 +0000

    ♻️  refactor(api): create bootstrap helper

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,62 +1,24 @@
-import { readFileSync } from 'fs';
-
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import cookieParser from 'cookie-parser';
 
-import { AppConfig } from './common/config/app.config';
+import { BootstrapHelper } from './common/utils/boostrap.helper';
 import { AppModule } from './modules/app/app.module';
 
-async function bootstrap(): Promise<{
-	protocol: string;
-	domain: string;
-	port: number;
-	globalPrefix: string;
-}> {
-	const httpsOptions = {
-		key: readFileSync('certs/set-selfsigned.key'),
-		cert: readFileSync('certs/set-selfsigned.crt'),
-	};
-
-	const app = await NestFactory.create(AppModule, { httpsOptions });
-
-	app.use(cookieParser());
-	app.enableCors({ origin: true, credentials: true });
-
-	const configService = app.get(ConfigService);
-	const config: AppConfig = configService.get<AppConfig>('app', {
-		infer: true,
-	});
-
-	app.setGlobalPrefix(config.prefix);
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-			transform: true,
-		}),
-	);
-
-	await app.listen(3000, '0.0.0.0');
+async function bootstrap(): Promise<void> {
+	const app = await NestFactory.create(AppModule, BootstrapHelper.nestApplicationOptions);
 
 	useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
-	return {
-		protocol: config.protocol,
-		domain: config.domain,
-		port: config.port,
-		globalPrefix: config.prefix,
-	};
+	const appConfig = BootstrapHelper.appConfig(app);
+
+	app.setGlobalPrefix(appConfig.prefix);
+	app.useGlobalPipes(BootstrapHelper.validationPipe);
+	app.enableCors({ origin: true, credentials: true, methods: 'GET,POST' });
+	app.use(cookieParser());
+
+	await app.listen(3000, '0.0.0.0', () => BootstrapHelper.logAppBoostrap(app));
 }
 
-bootstrap()
-	.then(({ protocol, domain, port }) => {
-		const playgroundUrl = `${protocol}://${domain}:${port}/graphql`;
-
-		Logger.log(
-			`🚀 GraphQL Playground ready at ${playgroundUrl}, started in ${process.uptime().toFixed(3)}s`,
-			'Bootstrap',
-		);
-	})
-	.catch((e) => Logger.error(e.message, e));
+bootstrap().catch((e) => Logger.error(e.message, e));
