5ef583776f451e5f0cb9f79b69f9ca1171bd0a56
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-07-06 22:48:41 +0000

    ♻️  refactor(app): improve shadows

--- apps/app/src/pages/example/shared/components/menu/example-menu.component.ts
+++ apps/app/src/pages/example/shared/components/menu/example-menu.component.ts
@@ -1,24 +1,35 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
 	standalone: true,
 	selector: 'set-example-menu',
-	imports: [RouterModule],
+	imports: [RouterModule, NgTemplateOutlet],
 	template: `
-		<nav>
-			<ul class="flex-row my-md gap-sm">
-				<li class="round-sm p-xs px-sm surface-vivid"><a routerLink="/example">Example</a></li>
-
-				<li class="round-sm p-xs px-sm surface-vivid">
-					<a routerLink="/example/typographies">Typographies</a>
-				</li>
-
-				<li class="round-sm p-xs px-sm surface-vivid">
-					<a routerLink="/example/shadows">shadows</a>
-				</li>
-			</ul>
+		<nav class="flex-row my-md gap-sm">
+			<ng-container
+				*ngTemplateOutlet="button; context: { text: 'Example', link: '/example' }"
+			></ng-container>
+			<ng-container
+				*ngTemplateOutlet="button; context: { text: 'Typographies', link: '/example/typographies' }"
+			></ng-container>
+			<ng-container
+				*ngTemplateOutlet="button; context: { text: 'Shadows', link: '/example/shadows' }"
+			></ng-container>
 		</nav>
+
+		<ng-template #button let-text="text" let-link="link">
+			<a
+				class="round-sm p-xs px-sm"
+				[class]="active.isActive ? 'weight-xxl surface-serene' : 'surface-vivid'"
+				routerLinkActive
+				#active="routerLinkActive"
+				[routerLinkActiveOptions]="{ exact: true }"
+				[routerLink]="link"
+				>{{ text }}</a
+			>
+		</ng-template>
 	`,
 })
 export default class ExampleMenuComponent {}
