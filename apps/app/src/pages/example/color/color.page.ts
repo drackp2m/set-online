8c367c9e9657a3998a4f7a8481272f66e64cadc1
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-07-07 18:43:53 +0000

    ♻️  refactor: use ngFor on colors

--- apps/app/src/pages/example/color/color.page.ts
+++ apps/app/src/pages/example/color/color.page.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 import ExampleMenuComponent from '../shared/components/menu/example-menu.component';
@@ -6,6 +7,8 @@ import ExampleMenuComponent from '../shared/components/menu/example-menu.compone
 	standalone: true,
 	templateUrl: './color.page.html',
 	styleUrls: ['./color.page.scss'],
-	imports: [ExampleMenuComponent],
+	imports: [ExampleMenuComponent, NgFor],
 })
-export default class ColorPage {}
+export default class ColorPage {
+	colors = ['cosmic', 'radiant', 'harmony', 'vivid', 'serene'];
+}
