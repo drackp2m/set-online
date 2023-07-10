3e68c4cac8626f4b705f9d5e956ee1d4c45cc413
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-07-10 01:04:08 +0000

    ✨ feat(app): enable to put cards rotated

--- apps/app/src/shared/components/card/card.component.ts
+++ apps/app/src/shared/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
 
 import { CardColorEnum, CardShadingEnum, CardShapeEnum } from '@set-online/api-definitions';
 
@@ -12,13 +12,56 @@ import { CardShapeComponent } from '../card-shape/card-shape.component';
 	styleUrls: ['./card.component.scss'],
 	imports: [NgIf, NgFor, CardShapeComponent],
 })
-export class CardComponent {
+export class CardComponent implements AfterViewInit, OnDestroy {
+	private resizeObserver!: ResizeObserver;
+	@ViewChild('card') card!: ElementRef<HTMLDivElement>;
+
 	@Input({ required: true }) shape!: keyof typeof CardShapeEnum;
 	@Input({ required: true }) color!: keyof typeof CardColorEnum;
 	@Input({ required: true }) shading!: keyof typeof CardShadingEnum;
 	@Input({ required: true }) count!: number;
+	@Input() rotate = false;
+
+	constructor(private elementRef: ElementRef) {}
+
+	ngAfterViewInit() {
+		if (this.rotate) {
+			this.card.nativeElement.style.maxWidth = 'initial';
+			this.card.nativeElement.style.maxHeight = 'initial';
+
+			this.initResizeObserver();
+		}
+	}
+
+	ngOnDestroy() {
+		if (this.resizeObserver) {
+			this.resizeObserver.unobserve(this.elementRef.nativeElement);
+			this.resizeObserver.disconnect();
+		}
+	}
 
 	get repetitions(): number[] {
 		return Array.from({ length: this.count });
+	}
+
+	private initResizeObserver() {
+		this.resizeObserver = new ResizeObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.target === this.elementRef.nativeElement) {
+					this.recalculateSize();
+				}
+			});
+		});
+
+		this.resizeObserver.observe(this.elementRef.nativeElement);
+	}
+
+	private recalculateSize() {
+		const containerWidth = this.elementRef.nativeElement.offsetWidth;
+
+		this.elementRef.nativeElement.style.height = `${containerWidth / 0.666}px`;
+
+		this.card.nativeElement.style.width = `${containerWidth / 0.666}px`;
+		this.card.nativeElement.style.height = `${containerWidth}px`;
 	}
 }
