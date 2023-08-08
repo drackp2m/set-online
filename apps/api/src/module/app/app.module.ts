18f0e9363c2d37019e14560c44fe980ed70fb4df
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-08-08 22:31:58 +0000

    🧪 test(api): mock tests with jest-mock-extended

--- apps/api/src/module/app/app.module.ts
+++ apps/api/src/module/app/app.module.ts
@@ -18,13 +18,10 @@ import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-// const isTestIntegrationEnv = 'test-int' === process.env.NODE_ENV;
-
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			// envFilePath: isTestIntegrationEnv ? 'apps/api/.env.test-int' : '.env',
 			load: [appConfig, databaseConfig, jwtConfig],
 		}),
 		MikroOrmModule.forRootAsync({
