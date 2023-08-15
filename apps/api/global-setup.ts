54020f3dbd2a5ace03495da3c9817f3c706fbbf8
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-08-15 20:26:48 +0000

    🧪 test(api): improve integration tests (test-setup and global-setups)

--- apps/api/global-setup.ts
+++ apps/api/global-setup.ts
@@ -1,7 +1,3 @@
-/* eslint-disable */
-
-var __TEARDOWN_MESSAGE__: string;
-
 module.exports = async function () {
 	// Start services that that the app needs to run (e.g. database, docker-compose, etc.).
 	console.log('\nSetting up...\n');
