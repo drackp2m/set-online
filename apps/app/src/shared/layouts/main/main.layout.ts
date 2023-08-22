5ed472ff918ba0d70d0fbdcd20d2f5a05857e1f2
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-08-22 19:39:15 +0000

    ✨ feat(api,app): add user store and getUserInfo query on userResolver

--- apps/app/src/shared/layouts/main/main.layout.ts
+++ apps/app/src/shared/layouts/main/main.layout.ts
@@ -1,7 +1,10 @@
-import { NgTemplateOutlet } from '@angular/common';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
 
+import { CurrentUserStore } from '../../../stores/current-user.store';
 import { MediaDebugComponent } from '../../components/media-debug/media-debug.component';
 import { ApiClient } from '../../services/api-client.service';
 
@@ -9,13 +12,18 @@ import { ApiClient } from '../../services/api-client.service';
 	standalone: true,
 	templateUrl: './main.layout.html',
 	styleUrls: ['./main.layout.scss'],
-	imports: [RouterOutlet, RouterModule, NgTemplateOutlet, MediaDebugComponent],
+	imports: [RouterOutlet, RouterModule, NgTemplateOutlet, MediaDebugComponent, NgIf],
 })
 export default class MainLayout {
+	user = toSignal(this.currentUserStore.state$.pipe(map((state) => state.data)));
+
 	constructor(
 		private readonly apiClient: ApiClient,
 		private readonly router: Router,
-	) {}
+		private readonly currentUserStore: CurrentUserStore,
+	) {
+		this.currentUserStore.fetchData();
+	}
 
 	logout() {
 		return () =>
