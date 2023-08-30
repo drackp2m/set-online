9875c46a98034838180c32fbed6bf12863a9d3d8
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-08-30 19:43:29 +0000

    🧪 test(api): editable-date specs

---
+++ apps/api/src/shared/util/editable-date.spec.ts
@@ -0,0 +1,114 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { GenerateNowDateUseCase } from '../use-case/generate-now-date.use-case';
+
+import { EditableDate } from './editable-date';
+
+describe('EditableDate', () => {
+	let util: EditableDate;
+
+	const fakeDate = new Date('2023-11-13T23:41:19.846Z');
+
+	jest.spyOn(GenerateNowDateUseCase, 'execute').mockReturnValue(fakeDate);
+
+	beforeAll(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [EditableDate],
+		}).compile();
+
+		util = module.get<EditableDate>(EditableDate);
+	});
+
+	it('should be defined', () => {
+		expect(util).toBeDefined();
+	});
+
+	describe('initialization', () => {
+		it('should return now date when the constructor is called without parameters', async () => {
+			const result = new EditableDate().toISOString();
+
+			expect(result).toStrictEqual('2023-11-13T23:41:19.846Z');
+		});
+
+		it('should replace year when call set method', async () => {
+			const result = new EditableDate().set('year', 2019).toISOString();
+
+			expect(result).toStrictEqual('2019-11-13T23:41:19.846Z');
+		});
+
+		it('should replace month when call set method', async () => {
+			const result = new EditableDate().set('month', 6).toISOString();
+
+			expect(result).toStrictEqual('2023-06-13T23:41:19.846Z');
+		});
+
+		it('should replace day when call set method', async () => {
+			const result = new EditableDate().set('day', 12).toISOString();
+
+			expect(result).toStrictEqual('2023-11-12T23:41:19.846Z');
+		});
+
+		it('should replace hour when call set method', async () => {
+			const result = new EditableDate().set('hour', 21).toISOString();
+
+			expect(result).toStrictEqual('2023-11-13T21:41:19.846Z');
+		});
+
+		it('should replace minute when call set method', async () => {
+			const result = new EditableDate().set('minute', 66).toISOString();
+
+			expect(result).toStrictEqual('2023-11-14T00:06:19.846Z');
+		});
+
+		it('should replace second when call set method', async () => {
+			const result = new EditableDate().set('second', 42).toISOString();
+
+			expect(result).toStrictEqual('2023-11-13T23:41:42.846Z');
+		});
+
+		it('should replace millisecond when call set method', async () => {
+			const result = new EditableDate().set('millisecond', 360).toISOString();
+
+			expect(result).toStrictEqual('2023-11-13T23:41:19.360Z');
+		});
+
+		it('should replace several values when calling the set method multiple times', async () => {
+			const result = new EditableDate().set('year', 1999).set('hour', 9).toISOString();
+
+			expect(result).toStrictEqual('1999-11-13T09:41:19.846Z');
+		});
+
+		it('should edit several values when calling the set method multiple times', async () => {
+			const result = new EditableDate().edit('month', -1).edit('second', 14).toISOString();
+
+			expect(result).toStrictEqual('2023-10-13T23:41:33.846Z');
+		});
+
+		it('should set and edit several values when calling methods multiple times', async () => {
+			const result = new EditableDate()
+				.set('year', 2048)
+				.edit('month', -1)
+				.set('second', 14)
+				.edit('second', 14)
+				.toISOString();
+
+			expect(result).toStrictEqual('2048-10-13T23:41:28.846Z');
+		});
+
+		it('should return the date specified in the constructor', async () => {
+			const fakeDate = new Date('2023-01-01T00:00:00.000Z');
+
+			const result = new EditableDate(fakeDate).toISOString();
+
+			expect(result).toStrictEqual(fakeDate.toISOString());
+		});
+
+		it('should set and edit several values of the date specified in the constructor', async () => {
+			const fakeDate = new Date('2023-01-01T00:00:00.000Z');
+
+			const result = new EditableDate(fakeDate).set('hour', 48).edit('day', -1).toISOString();
+
+			expect(result).toStrictEqual('2023-01-02T00:00:00.000Z');
+		});
+	});
+});
---
+++ apps/api/src/shared/util/editable-date.ts
@@ -0,0 +1,61 @@
+import { GenerateNowDateUseCase } from '../use-case/generate-now-date.use-case';
+
+type ManipulationDateAttribute =
+	| 'year'
+	| 'month'
+	| 'day'
+	| 'hour'
+	| 'minute'
+	| 'second'
+	| 'millisecond';
+
+export class EditableDate extends Date {
+	constructor(initialDate = GenerateNowDateUseCase.execute()) {
+		super(initialDate);
+	}
+
+	edit(attribute: ManipulationDateAttribute, value: number): this {
+		this.manipulatedDate(attribute, value);
+
+		return this;
+	}
+
+	set(attribute: ManipulationDateAttribute, value: number): this {
+		this.manipulatedDate(attribute, value, true);
+
+		return this;
+	}
+
+	private manipulatedDate(
+		attribute: ManipulationDateAttribute,
+		value: number,
+		replace = false,
+	): void {
+		const currentValue = this[`getUTC${this.manipulationDateAttributeMethod(attribute)}`]();
+
+		this[`setUTC${this.manipulationDateAttributeMethod(attribute)}`](
+			(replace ? 0 : currentValue) + this.fixValue(attribute, value, replace),
+		);
+	}
+
+	private manipulationDateAttributeMethod(attribute: ManipulationDateAttribute): string {
+		switch (attribute) {
+			case 'year':
+				return 'FullYear';
+			case 'month':
+				return 'Month';
+			case 'day':
+				return 'Date';
+			default:
+				return `${attribute.charAt(0).toUpperCase() + attribute.slice(1)}s`;
+		}
+	}
+
+	private fixValue(attribute: ManipulationDateAttribute, value: number, replace: boolean): number {
+		if (replace && attribute === 'month') {
+			return value - 1;
+		}
+
+		return value;
+	}
+}
