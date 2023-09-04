3002955f44c9716c0c5333406d615bd00c35cb55
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-09-04 20:20:53 +0000

    🧪 test(api): add CheckPassword UseCase Spec

---
+++ apps/api/src/shared/use-case/check-password.use-case.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { CheckPasswordUseCase } from './check-password.use-case';
+
+describe('CheckPasswordUseCase', () => {
+	let useCase: CheckPasswordUseCase;
+
+	beforeAll(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [CheckPasswordUseCase],
+		}).compile();
+
+		useCase = await module.resolve<CheckPasswordUseCase>(CheckPasswordUseCase);
+	});
+
+	it('should be defined', () => {
+		expect(useCase).toBeDefined();
+	});
+
+	describe('execute', () => {
+		it('should return false when password not match', async () => {
+			const passwordMatch = await useCase.execute(
+				'password',
+				'$2a$12$iOm3t8Z8HeY1didYbQdbnuYepl1zCaDB6linWecn8HKpwPaZ/CMum',
+			);
+
+			expect(passwordMatch).toStrictEqual(false);
+		});
+
+		it('should return true when password match', async () => {
+			const passwordMatch = await useCase.execute(
+				'password',
+				'$2a$12$iOm3t8Z8HeY1didYbQdbnuYegl1zCaDB6linWecn8HKpwPaZ/CMum',
+			);
+
+			expect(passwordMatch).toStrictEqual(true);
+		});
+	});
+});
