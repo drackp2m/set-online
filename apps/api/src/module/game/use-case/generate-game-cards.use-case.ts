680731af7ba1e513a16ebfef1a994c43a96af3e6
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-09-04 19:22:20 +0000

    ✨ feat(api): create generateGameCards UseCase

---
+++ apps/api/src/module/game/use-case/generate-game-cards.use-case.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+
+import { cardColors } from '../definition/card-color.enum';
+import { CardPropertiesEnum } from '../definition/card-properties.enum';
+import { cardShadings } from '../definition/card-shading.enum';
+import { cardShapes } from '../definition/card-shape.enum';
+
+@Injectable()
+export class GenerateGameCardsUseCase {
+	async execute(): Promise<string[]> {
+		const cards = [];
+
+		cardShapes.forEach((_shapeValue, shapeKey) => {
+			const shape = `:${CardPropertiesEnum['shape']}${shapeKey}:`;
+
+			cardColors.forEach((_colorValue, colorKey) => {
+				let color = shape;
+
+				color += `:${CardPropertiesEnum['color']}${colorKey}:`;
+
+				[1, 2, 3].forEach((numberKey) => {
+					let number = color;
+
+					number += `:${CardPropertiesEnum['number']}${numberKey}:`;
+
+					cardShadings.forEach((_shadingValue, shadingKey) => {
+						let shading = number;
+
+						shading += `:${CardPropertiesEnum['shading']}${shadingKey}:`;
+
+						cards.push(shading);
+					});
+				});
+			});
+		});
+
+		return cards;
+	}
+}
