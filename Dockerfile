d8cf86ddaea7e28df6cc10f6f749f7aed051c941
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2023-09-26 22:29:24 +0000

    🎒 chore: update docker node image, add jest global setup to unit tests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20.2-alpine3.17 AS base
+FROM node:20.7-alpine3.18 AS base
 
 RUN apk add --no-cache build-base python3
 
--- apps/api/global-teardown.ts
+++ apps/api/global-teardown.ts
@@ -1,8 +1,3 @@
-import { MikroORM } from '@mikro-orm/core';
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
-
 module.exports = async function () {
 	console.log(globalThis.__TEARDOWN_MESSAGE__);
-
-	await (globalThis.__MIKRO_ORM__ as MikroORM<PostgreSqlDriver>).close();
 };
