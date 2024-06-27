78899ab4442903a6ac010aca9c07fc937fe5ac51
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2024-06-27 23:43:57 +0000

    🐛 fix(app): finish authInterceptor logic

--- apps/app/src/app/shared/interceptors/auth.interceptor.ts
+++ apps/app/src/app/shared/interceptors/auth.interceptor.ts
@@ -10,7 +10,7 @@ import { Injectable, inject } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { catchError, filter, switchMap, take } from 'rxjs/operators';
+import { catchError, filter, skip, switchMap, take } from 'rxjs/operators';
 
 import { AuthStore } from '../../stores/auth.store';
 
@@ -24,49 +24,30 @@ export class AuthInterceptor implements HttpInterceptor {
 	private readonly API_LOGIN_URL = '/api/auth/login';
 
 	private readonly authStoreLoadingFinished$ = toObservable(this.authStore.isLoading).pipe(
+		skip(1),
 		filter((value) => value === false),
 	);
 
 	intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+		const tokensAreValid = this.authStore.data()?.tokensAreValid;
+
 		const urlsToIgnore = [this.API_REGISTER_URL, this.API_LOGIN_URL, this.API_REFRESH_SESSION_URL];
 
-		if (urlsToIgnore.some((url) => req.url.includes(url))) {
+		if (tokensAreValid === false || urlsToIgnore.some((url) => req.url.includes(url))) {
 			return next.handle(req);
 		}
 
 		return next.handle(req).pipe(
 			catchError((error: HttpEvent<unknown>) => {
 				return this.handleErrorOrEvent(req, next, error);
-				// if (error instanceof HttpErrorResponse === false) {
-				// 	return of(error);
-				// }
-
-				// const isUnauthorizedError = error.status === 401;
-
-				// if (isUnauthorizedError === true && this.authStore.data()?.tokensAreValid === undefined) {
-				// 	return this.retryRequest(req, next, error);
-				// }
-
-				// return of(error);
 			}),
 			switchMap((event) => {
 				return this.handleErrorOrEvent(req, next, event);
-				// console.log('map');
-				// if (event instanceof HttpResponse === false) {
-				// 	return of(event);
-				// }
-
-				// const isUnauthorizedError = event.body?.errors?.[0]?.message === 'Unauthorized';
-
-				// if (isUnauthorizedError === true && this.authStore.data()?.tokensAreValid === undefined) {
-				// 	return this.retryRequest(req, next, event);
-				// }
-
-				// return of(event);
 			}),
 		);
 	}
 
+	// ToDo => improve types of this method
 	handleErrorOrEvent(
 		req: HttpRequest<unknown>,
 		next: HttpHandler,
@@ -83,7 +64,7 @@ export class AuthInterceptor implements HttpInterceptor {
 			? event.status === 401
 			: event.body?.errors?.[0]?.message === 'Unauthorized';
 
-		if (isUnauthorizedError && this.authStore.data()?.tokensAreValid === undefined) {
+		if (isUnauthorizedError && this.authStore.data()?.tokensAreValid !== false) {
 			return this.retryRequest(req, next, event as HttpResponse<unknown>);
 		}
 
@@ -104,8 +85,6 @@ export class AuthInterceptor implements HttpInterceptor {
 			switchMap(() => {
 				if (this.authStore.data()?.tokensAreValid === false) {
 					void this.router.navigate(['/login']);
-
-					return new Observable<HttpEvent<unknown>>();
 				}
 
 				return next.handle(req);
