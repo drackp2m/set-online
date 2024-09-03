d52721f7548c6dc634575c45bd595cbfa2bb46c6
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2024-09-03 20:51:05 +0000

    🎒 chore: add comment

--- apps/api/src/module/auth/use-case/hash-password.use-case.ts
+++ apps/api/src/module/auth/use-case/hash-password.use-case.ts
@@ -2,6 +2,7 @@ import { genSalt, hash } from 'bcryptjs';
 
 export class HashPasswordUseCase {
 	async execute(password: string): Promise<string> {
+		// add comment
 		const salt = await genSalt(11);
 
 		return hash(password, salt);
