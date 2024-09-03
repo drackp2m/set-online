be8ba0d21badd705d928139780e5f98e8638e5ac
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2024-09-03 22:50:04 +0200

    Revert "Revert "Dev""

--- apps/api/src/module/auth/use-case/hash-password.use-case.ts
+++ apps/api/src/module/auth/use-case/hash-password.use-case.ts
@@ -2,7 +2,7 @@ import { genSalt, hash } from 'bcryptjs';
 
 export class HashPasswordUseCase {
 	async execute(password: string): Promise<string> {
-		const salt = await genSalt(12);
+		const salt = await genSalt(11);
 
 		return hash(password, salt);
 	}
