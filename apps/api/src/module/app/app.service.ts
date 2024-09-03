2aafcb2e99c3f39e3d6271caf0e39caf17894a86
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2024-09-03 22:42:03 +0200

    Revert "Dev"

--- apps/api/src/module/app/app.service.ts
+++ apps/api/src/module/app/app.service.ts
@@ -3,19 +3,11 @@ import { Cron } from '@nestjs/schedule';
 
 import { Message } from '@set-online/api-definitions';
 
-import { ConfigurationService } from '../../shared/module/config/configuration.service';
-
 @Injectable()
 export class AppService {
-	constructor(private readonly configService: ConfigurationService) {}
-
 	@Cron('42 * * * * *')
 	async handleCron() {
-		const apiConfig = this.configService.api;
-		const port = apiConfig.port ? `:${apiConfig.port}` : '';
-		const apiBaseUrl = `${apiConfig.protocol}://${apiConfig.domain}${port}`;
-
-		const response = await fetch(apiBaseUrl);
+		const response = await fetch('https://api.playsetonline.com');
 
 		console.log(await response.text());
 	}
