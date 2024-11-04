b1154ac88230e7f04fb248fd24f24bbef85cfe20
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2024-11-04 00:05:32 +0000

    🐛 fix: remove customized release-notes-generator

--- release.config.js
+++ release.config.js
@@ -19,59 +19,60 @@ module.exports = {
 				},
 			},
 		],
-		[
-			'@semantic-release/release-notes-generator',
-			{
-				writerOpts: {
-					commitsSort: ['subject', 'scope'],
-					transform: (commit) => {
-						const clonedCommit = { ...commit };
+		'@semantic-release/release-notes-generator',
+		// [
+		// 	'@semantic-release/release-notes-generator',
+		// 	{
+		// 		writerOpts: {
+		// 			commitsSort: ['subject', 'scope'],
+		// 			transform: (commit) => {
+		// 				const clonedCommit = { ...commit };
 
-						clonedCommit.notes.forEach((note) => {
-							note.title = 'BREAKING CHANGES';
-						});
+		// 				clonedCommit.notes.forEach((note) => {
+		// 					note.title = 'BREAKING CHANGES';
+		// 				});
 
-						if (clonedCommit.type === 'feat') {
-							clonedCommit.type = '✨ Features';
-						} else if (clonedCommit.type === 'style') {
-							clonedCommit.type = '🎨 Styles';
-						} else if (clonedCommit.type === 'test') {
-							clonedCommit.type = '🧪 Tests';
-						} else if (clonedCommit.type === 'refactor') {
-							clonedCommit.type = '♻️ Code Refactoring';
-						} else if (clonedCommit.type === 'fix') {
-							clonedCommit.type = '🐛 Bug Fixes';
-						} else if (clonedCommit.type === 'docs') {
-							clonedCommit.type = '📚 Documentation';
-						} else if (clonedCommit.type === 'perf') {
-							clonedCommit.type = '🚀 Performance Improvements';
-						} else if (clonedCommit.type === 'build') {
-							clonedCommit.type = '🏗️‍ Build System';
-						} else if (clonedCommit.type === 'ci') {
-							clonedCommit.type = '💻 Continuous Integration';
-						} else if (clonedCommit.type === 'Chore') {
-							clonedCommit.type = '🎒 Continuous Integration';
-						} else if (clonedCommit.type === 'revert') {
-							clonedCommit.type = '⏪ Reverts';
-						}
+		// 				if (clonedCommit.type === 'feat') {
+		// 					clonedCommit.type = '✨ Features';
+		// 				} else if (clonedCommit.type === 'style') {
+		// 					clonedCommit.type = '🎨 Styles';
+		// 				} else if (clonedCommit.type === 'test') {
+		// 					clonedCommit.type = '🧪 Tests';
+		// 				} else if (clonedCommit.type === 'refactor') {
+		// 					clonedCommit.type = '♻️ Code Refactoring';
+		// 				} else if (clonedCommit.type === 'fix') {
+		// 					clonedCommit.type = '🐛 Bug Fixes';
+		// 				} else if (clonedCommit.type === 'docs') {
+		// 					clonedCommit.type = '📚 Documentation';
+		// 				} else if (clonedCommit.type === 'perf') {
+		// 					clonedCommit.type = '🚀 Performance Improvements';
+		// 				} else if (clonedCommit.type === 'build') {
+		// 					clonedCommit.type = '🏗️‍ Build System';
+		// 				} else if (clonedCommit.type === 'ci') {
+		// 					clonedCommit.type = '💻 Continuous Integration';
+		// 				} else if (clonedCommit.type === 'Chore') {
+		// 					clonedCommit.type = '🎒 Continuous Integration';
+		// 				} else if (clonedCommit.type === 'revert') {
+		// 					clonedCommit.type = '⏪ Reverts';
+		// 				}
 
-						if (clonedCommit.scope === '*') {
-							clonedCommit.scope = '';
-						}
+		// 				if (clonedCommit.scope === '*') {
+		// 					clonedCommit.scope = '';
+		// 				}
 
-						if (typeof clonedCommit.hash === 'string') {
-							clonedCommit.shortHash = clonedCommit.hash.substring(0, 7);
-						}
+		// 				if (typeof clonedCommit.hash === 'string') {
+		// 					clonedCommit.shortHash = clonedCommit.hash.substring(0, 7);
+		// 				}
 
-						if (typeof clonedCommit.subject === 'string') {
-							clonedCommit.subject = clonedCommit.subject.substring(0, 72);
-						}
+		// 				if (typeof clonedCommit.subject === 'string') {
+		// 					clonedCommit.subject = clonedCommit.subject.substring(0, 72);
+		// 				}
 
-						return commit;
-					},
-				},
-			},
-		],
+		// 				return commit;
+		// 			},
+		// 		},
+		// 	},
+		// ],
 		'@semantic-release/changelog',
 		[
 			'@semantic-release/github',
